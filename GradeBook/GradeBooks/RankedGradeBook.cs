d06523eb68fea3bd84ca304c8d49de128a4c6cf2
Daniel <ll8083129@gmail.com>
2020-03-25 15:07:23 +0100

    stage 3 in progress

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,14 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minumum of 5 students to work.");
+            }
+            else if ()
+            return 'F';
+        }
     }
 }
